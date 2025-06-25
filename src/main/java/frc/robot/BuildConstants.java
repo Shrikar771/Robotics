628e80a9e07144a7af4eb95ba2ef5908d46db39c
Shrikar771 <shrikar.sankar11@gmail.com>
2025-06-25 15:12:30 -0500

    Initial commit

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "SteelTalonsLibrary";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "7fe94f961923e1b0cf76f3eca77091501670dc99";
+  public static final String GIT_DATE = "2025-05-26 10:51:33 CDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-05-26 10:55:46 CDT";
+  public static final long BUILD_UNIX_TIME = 1748274946552L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
